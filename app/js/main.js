6a7c60dace99f59edd983ae12f0d8ce4383772b3
Sergey Zakirov <erega74@gmail.com>
2020-02-19 01:41:29 +0500

    Сверстан блок 'latest works'

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,42 @@ var swiper = new Swiper('.header-slider__container', {
       },
     },
 });
+
+var swiper = new Swiper('.latest-works__slider-container', {
+  slidesPerView: 4,
+  spaceBetween: 15,
+  slidesPerGroup: 4,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1000: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      slidesPerGroup: 4,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      slidesPerGroup: 4,
+    },
+    550: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+      slidesPerGroup: 3,
+    },
+    410: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      slidesPerGroup: 2
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      slidesPerGroup: 1
+    }
+  }
+});
